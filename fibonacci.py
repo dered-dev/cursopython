a1d8a6c1177238d67a899d0cbc31d6f6ab50bd1e
jorge luis camarillo cristobal <jlcc170@gmail.com>
2020-10-03 01:38:58 -0500

    ADD Fibonacci function

--- fibonacci.py
+++ fibonacci.py
@@ -1,19 +1,26 @@
 """Fibonacci function"""
 
-count_number = int(input("¿Cuántos numeros necesitas? "))
-# one and two values
-number1 = 0
-number2 = 1
-counter = 0
+def fibonacciSequence(count_number) :
+    
+    # one and two values
+    number1 = 0
+    number2 = 1
+    counter = 0
 
-# positive number
-if count_number <= 0:
-    print("Introduce un número positivo")
-else:
-    print("Secuencia de Fibonacci hasta ",count_number,": ")
-    while counter < count_number :
-        print(number1)
-        nextNumber = number1 + number2
-        number1 = number2
-        number2 = nextNumber
-        counter += 1
+    # positive number
+    if count_number <= 0:
+        print("Introduce un número positivo")
+    else:
+        print("Secuencia de Fibonacci hasta ",count_number,": ")
+        while counter < count_number :
+            print(number1)
+            nextNumber = number1 + number2
+            number1 = number2
+            number2 = nextNumber
+            counter += 1
+
+# get user limit
+userNumber = int(input("¿Cuántos numeros necesitas? "))
+
+# invoque function
+fibonacciSequence(userNumber)
